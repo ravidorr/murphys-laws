0f9e5837dc135b719cc77c67ccfd7c8f44a1934b
Raanan Avidor <ravidor@pendo.io>
2026-01-11 09:33:17 +0200

    style: revert icon addition to match clean section style

--- web/src/views/home.js
+++ web/src/views/home.js
@@ -44,10 +44,7 @@ export function renderHome(el, lawOfTheDay, categories, onNavigate) {
 
     categoriesSection.innerHTML = `
       <div class="section-header">
-        <h2 class="section-title">
-          <span class="icon" data-icon="list" aria-hidden="true"></span>
-          Popular Categories
-        </h2>
+        <h2 class="section-title">Popular Categories</h2>
       </div>
       <div class="section-body">
         <div class="category-grid">
